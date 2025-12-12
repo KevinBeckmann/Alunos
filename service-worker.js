a9f665328a2e3cd96628ae3e48216e357fdf1201
Kevin Beckmann <kevinbeckmannm@gmail.com>
2025-12-12 20:26:02 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "alunos-pwa-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./professor.html",
+        "./reservaprofessor.html",
+        "./logotipo.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
